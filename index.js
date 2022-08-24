b0649c87e75dd8a310a4778b2ac54ff35ae49661
dankiprop <dankiproh@gmail.com>
2022-08-24 17:46:34 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers= ([Antonia, Nuru,Amari,Mo])=> {
+    return  (['Antonia', 'Nuru'])
+}
+
+const returnLastTwoDrivers= ([Antonia, Nuru,Amari,Mo])=> {
+    return  (['Amari', 'Mo'])
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const a=5
+const b=5
+function createFareMultiplier() {
+    return function multiply(a,b) {
+        return 25
+    }
+}
+
+function fareDoubler(fare) {
+    return (fare) * 2
+}
+function fareTripler(fare) {
+    return (fare) * 3
+}
+const selectDifferentDrivers = ([Antonia, Nuru,Amari,Mo],returnLastTwoDrivers) => { return returnLastTwoDrivers(['Amari', 'Mo']) } 
